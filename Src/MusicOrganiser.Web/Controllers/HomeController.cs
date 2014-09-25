3d7c31e2755748dd11ab34524f53b453ae4e3e5c
buhbuh <JuggleDrum@hotmail.co.uk>
2014-09-25 20:35:17 +0100

    Chose the United bootstrap theme from: http://bootswatch.com/united/

--- Src/MusicOrganiser.Web/Controllers/HomeController.cs
+++ Src/MusicOrganiser.Web/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace MusicOrganiser.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
