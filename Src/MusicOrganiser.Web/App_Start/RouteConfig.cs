11db74268cd1694c1ec80115b8f3501a1bc98f46
buhbuh <JuggleDrum@hotmail.co.uk>
2014-10-04 11:03:53 +0100

    Catch all MVC route, so that we can defer routing to AngularJS

--- Src/MusicOrganiser.Web/App_Start/RouteConfig.cs
+++ Src/MusicOrganiser.Web/App_Start/RouteConfig.cs
@@ -13,11 +13,9 @@ namespace MusicOrganiser.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute("CatchAll", "{*url}", new { controller = "Home", action = "Index" });
         }
     }
 }
