4b8a30aeca8f59a940b15c190261bc3b30dbeed6
buhbuh <JuggleDrum@hotmail.co.uk>
2014-10-04 18:15:53 +0100

    Alert on error script so I can see when Angular has failed.

---
+++ Src/MusicOrganiser.Web/Scripts/AlertOnError.js
@@ -0,0 +1,15 @@
+﻿
+(function () {
+    function overwrite(functionName) {
+
+        console["old_" + functionName] = console[functionName];
+
+        console[functionName] = function (message) {
+            alert(message);
+            console["old_" + functionName](message);
+        }
+    }
+
+    overwrite("debug");
+    overwrite("error");
+})();
